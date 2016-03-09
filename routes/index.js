5ca0abf38ad4e69bb72157a44a08d6194dc45c78
Phil Varner <philvarnerlp9@gmail.com>
2016-03-09 18:34:35 +0000

    rest of changes

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,22 @@ router.get('/', function(req, res, next) {
   res.sendFile('weather.html', { root:  'public' });
 });
 
+router.get('/test1', function(req, res, next) {
+  res.sendFile('test1.html', { root:  'public' });
+});
+
+router.get('/test2', function(req, res, next) {
+  res.sendFile('test2.txt', { root:  'public' });
+});
+
+router.get('/test3', function(req, res, next) {
+  res.sendFile('test3.gif', { root:  'public' });
+});
+
+router.get('/test4', function(req, res, next) {
+  res.sendFile('test4.jpg', { root:  'public' });
+});
+
 router.get('/getcity',function(req,res,next) {
 
 	var myRe = new RegExp("^" + req.query.q);
@@ -22,60 +38,20 @@ router.get('/getcity',function(req,res,next) {
           } 
 	    }
 	    res.status(200).json(jsonresult);
-	  });
+	});
+});
 
-
-  //   console.log("In getcity route");
-  //   console.log(1);
-  //   var fs = require('fs');
-  //   fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
-	 //    if(err) throw err;
-	 //    var cities = data.toString().split("\n");
-	 //    for(var i = 0; i < cities.length; i++) {
-	 //      	console.log(cities[i]);
-	 //      	console.log(req.query.q);
-	 //      	console.log(2);
-	 //      	var myRe = new RegExp("^" + req.query.q);
-	 //  		console.log(myRe);
-	 //  		console.log(3);
-
-	 //    	var result = cities[i].search(myRe);
-		//   	if(result != -1) {
-		//     	console.log(cities[i]);
-		//     	console.log(4);
-		//   	}
-
-		//   	var jsonresult = [];
-		//   	console.log('number of cities' + cities.length);
-	 //        for(var i = 0; i < cities.length; i++) {
-	 //          	var result = cities[i].search(myRe); 
-	 //          	if(result != -1) {
-	 //           		console.log(cities[i]);
-	 //           		console.log(5);
-	 //           		jsonresult.push({city:cities[i]});
-	 //          	} 
-	 //        }   
-	 //        console.log(jsonresult);
-	 //        console.log(6);
-	 //        res.status(200).json(jsonresult);
-		// }
-  //   });
+router.get('/getskiresorts', function(req,res,next) {
+	fs.readFile(__dirname + '/topskiresorts.txt', function(err,data) {
+		if (err) throw err;
+		var resorts = data.toString().split("\n");
+		var jsonresult = [];
+		for(var i = 0; i < resorts.length; i++) { 
+	   		jsonresult.push({resort:resorts[i]});
+	    }
+	    console.log(jsonresult);
+	    res.status(200).json(jsonresult); 
+	});
 });
 
 module.exports = router;
-
-
-// var myRe = new RegExp("^", + req.query.q);
-
-	// fs.readFile(__dirname + '/cities.dat.txt', function (err,data) {
-	// 	if (err) throw err;
-	// 	var jsonresult = [];
-	// 	var cities = data.toString().split("\n");
-	// 	for (var i = 0; i < cities.length; i++) {
-	// 		var result = cities[i].search(myRe);
-	// 		if (result != -1) {
-	// 			jsonresult.push({city: cities[i]});
-	// 		}
-	// 	}
-	// 	res.status(200).json(jsonresult);
-	// })
